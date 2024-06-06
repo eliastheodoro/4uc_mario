26324159e46b12165f5d5426c634ba5247334502
Elias Theodoro <elias.theodoro@blueshift.com.br>
2024-06-06 09:31:45 -0300

    melhoras na logica

--- main.py
+++ main.py
@@ -4,13 +4,12 @@ import json
 uri = 'https://api.mercadolibre.com/oauth/token'
 client_id = '1196296498468732'
 client_secret = 'Ef4utzldvpALfFUygJZdJxzHooRen3Ng'
-# code = 'TG-6655f0d7836be00001b3f403-17415698'
 redirect_uri = 'https://www.google.com'
 nome_arquivo = 'Z:\\Dados\\OneDrive\\Documentos\\codigo\\4uc_mario\\4uc_mario\\tokens.json'
 nome_arquivo_output = 'Z:\\Dados\\OneDrive\\Documentos\\codigo\\4uc_mario\\4uc_mario\\out.json'
 
 
-def geraToken(uri, client_id, client_secret, code=None, refresh_token=None, redirect_uri=None, access_token=None):
+def geraToken(uri, client_id, client_secret, code=None, refresh_token=None, redirect_uri=None):
     param = {
         'client_id': client_id,
         'client_secret': client_secret,
@@ -39,7 +38,8 @@ def geraToken(uri, client_id, client_secret, code=None, refresh_token=None, redi
     else:
         # Coloque aqui o código para salvar o access token e o refresh token (se aplicável)
         # Salvar as variáveis em um arquivo
-        salvar_variaveis_em_json(nome_arquivo, access_token=access_token, refresh_token=refresh_token)
+        data = response.json()
+        salvar_variaveis_em_json(nome_arquivo, access_token=data.get("access_token"), refresh_token=data.get("refresh_token"))
         return response.json()
 
 def salvar_variaveis_em_json(nome_arquivo, **variaveis):
@@ -55,37 +55,38 @@ def carregar_variaveis_do_json(nome_arquivo):
         print(f"Arquivo '{nome_arquivo}' não encontrado.")
         return {}
 
-def salvar_output(nome_arquivo_output, **variaveis):
-    with open(nome_arquivo, 'w') as file:
-        json.dump(variaveis, file)
-
 def main():
 
+    # codigo que eh gerado manualmente. Somente para situacoes em que o refresh_token é perdido. Se for atribuido um valor a def geraToken vai regerar o refresh.
+    code = None
 
     # Carregar as variáveis do arquivo 'tokens.json'
     loaded_data = carregar_variaveis_do_json(nome_arquivo)
     access_token = loaded_data.get("access_token")
     refresh_token = loaded_data.get("refresh_token")
 
-    url = f"https://api.mercadolibre.com/sites/MLB/search?seller_id=1317250329"
+    url = f"https://api.mercadolibre.com/orders/search?seller=1317250329" #
 
     headers = {
     'Authorization': f'Bearer {access_token}'
     }
 
-# teste consulta api
+    # teste consulta api
     response = requests.request("GET", url, headers=headers)
 
+    if response.status_code == 400 or response.status_code == 401 or response.status_code == 401:
+        print(response.json())
+        print('Regerando Bearer code a partir do refresh token...')
+        geraToken(uri, client_id, client_secret, code=code, refresh_token=refresh_token, redirect_uri=redirect_uri)
+        #refaz a chamada na api
+        response = requests.request("GET", url, headers=headers)
+        print(response.json())
 
-    if response.status_code == 400:
-        print('Erro 400: Requisição inválida. Você deve gerar outro CODE ou usar um refresh token válido.')
-        print('Tentando refresh_token...')
-        geraToken(uri, client_id, client_secret, refresh_token=refresh_token, redirect_uri=redirect_uri)
-        return response.json()
+        return {}
     else:
-        print("teste")
-        salvar_output(nome_arquivo_output, access_token=access_token, refresh_token=refresh_token)
+        salvar_variaveis_em_json(nome_arquivo_output, response=response.json())
         # return response.json()
         print(response.text)
+        return {}
 
 main()
